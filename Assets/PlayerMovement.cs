4b575eece099d5c1d6610796888b34af4f1703d0
dglenn3 <35244607+dglenn3@users.noreply.github.com>
2020-11-10 22:42:15 -0700

    Add player movement
    Added left, right, jump and crouch
    Added slippery material to player

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public float horizontalMove = 0f;
+
+    public float runSpeed = 20f;
+
+    bool jump = false;
+
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        jump = false;
+    }
+}
