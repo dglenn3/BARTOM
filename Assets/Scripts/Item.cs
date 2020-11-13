32412f40d2391b8d17ffbcce384e9ec7835ff023
dglenn3 <35244607+dglenn3@users.noreply.github.com>
2020-11-12 22:03:30 -0700

    Add item and item grab
    Added gem to scene
    Added grab gem

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private bool canPickUp = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canPickUp && Input.GetButtonUp("Grab"))
+        {
+            Grab();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            canPickUp = false;
+        }
+    }
+
+    void Grab()
+    {
+        Destroy(gameObject);
+    }
+}
